373bdd3e5556b77dfafa4afb22bf09ab35579000
Simmo Saan <simmo.saan@gmail.com>
2015-09-13 15:26:17 +0300

    Simplify serialization

--- opencv_test/main.cpp
+++ opencv_test/main.cpp
@@ -18,16 +18,20 @@ int main()
 
     bool mirror_h = false, mirror_v = false;
 
-    pair<int, int> hval(0, 179);
-    pair<int, int> sval(0, 255);
-    pair<int, int> vval(0, 255);
+    Scalar_<int> lower(0, 0, 0), upper(179, 255, 255);
 
-    createTrackbar("hmin", "Video 1", &hval.first, 179);
-    createTrackbar("hmax", "Video 1", &hval.second, 179);
-    createTrackbar("smin", "Video 1", &sval.first, 255);
-    createTrackbar("smax", "Video 1", &sval.second, 255);
-    createTrackbar("vmin", "Video 1", &vval.first, 255);
-    createTrackbar("vmax", "Video 1", &vval.second, 255);
+    {
+        FileStorage fs("calib.yml", FileStorage::READ);
+        fs["lower"] >> lower;
+        fs["upper"] >> upper;
+    }
+
+    createTrackbar("hmin", "Video 1", &lower[0], 179);
+    createTrackbar("hmax", "Video 1", &upper[0], 179);
+    createTrackbar("smin", "Video 1", &lower[1], 255);
+    createTrackbar("smax", "Video 1", &upper[1], 255);
+    createTrackbar("vmin", "Video 1", &lower[2], 255);
+    createTrackbar("vmax", "Video 1", &upper[2], 255);
 
     while (1)
     {
@@ -41,11 +45,9 @@ int main()
 
         Mat hsv;
         cvtColor(frame, hsv, CV_BGR2HSV);
-        auto lower = Scalar(hval.first, sval.first, vval.first);
-        auto upper = Scalar(hval.second, sval.second, vval.second);
 
         Mat mask;
-        inRange(hsv, lower, upper, mask);
+        inRange(hsv, Scalar(lower), Scalar(upper), mask);
 
         Mat filtered;
         frame.copyTo(filtered, mask);
@@ -56,7 +58,7 @@ int main()
         {
             case 27: // ESC
             case 'q':
-                return EXIT_SUCCESS;
+                goto quit;
             case 'h':
                 mirror_h ^= 1;
                 break;
@@ -64,9 +66,14 @@ int main()
                 mirror_v ^= 1;
                 break;
         }
+    }
 
-        auto pixel = frame.at<Vec3b>(0, 0);
-        cout << int(pixel[0]) << "\t" << int(pixel[1]) << "\t" << int(pixel[2]) << endl;
+quit:
+
+    {
+        FileStorage fs("calib.yml", FileStorage::WRITE);
+        fs << "lower" << lower;
+        fs << "upper" << upper;
     }
 
     return EXIT_SUCCESS;
