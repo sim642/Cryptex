986990f03d7814e5482a906da1f77b2f976b1950
Simmo Saan <simmo.saan@gmail.com>
2015-09-18 14:59:25 +0300

    Create serial_device and use streams

--- motor_test/main.cpp
+++ motor_test/main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/iostreams/stream.hpp>
+#include "serial_device.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
 
 int main()
 {
     boost::asio::io_service io;
-    boost::asio::serial_port serial(io, "/dev/ttyACM1");
-    serial.set_option(boost::asio::serial_port_base::baud_rate(115200));
+    boost::asio::serial_port port(io, "/dev/ttyACM1");
+    port.set_option(boost::asio::serial_port_base::baud_rate(115200));
 
-    string str = "sd-10\n";
-	boost::asio::write(serial, boost::asio::buffer(str.c_str(), str.length()));
+    serial_device dev(port);
+    boost::iostreams::stream<serial_device> serial(dev);
+
+    serial << "?" << endl;
+    string text;
+    serial >> text;
+    cout << text << endl;
+
+    while (true)
+    {
+        serial << "sd10" << endl;
+
+        boost::asio::deadline_timer timer(io, boost::posix_time::seconds(1));
+        timer.wait();
+    }
     return 0;
 }
