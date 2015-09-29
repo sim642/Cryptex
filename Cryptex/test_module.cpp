07cd0a2cfa9ff5f51b02134e444bd30447739f70
Simmo Saan <simmo.saan@gmail.com>
2015-09-29 16:35:55 +0300

    Test omnidrive over RS485

--- Cryptex/test_module.cpp
+++ Cryptex/test_module.cpp
@@ -8,6 +8,8 @@
 #include <chrono>
 #include <thread>
 
+#include "driver.hpp"
+
 using namespace std;
 
 test_module::test_module()
@@ -25,15 +27,23 @@ module::type test_module::run(const module::type &prev_module)
 	boost::asio::io_service io;
 
 	rs485_dongle dongle(io, "/dev/ttyUSB0");
-	device_controller *control = dongle[1];
+	driver d(dongle);
+
+	for (int i = 0; i < 4; i++)
+	{
+		d.omni(50, i * 90);
+		this_thread::sleep_for(chrono::seconds(1));
+	}
+
+	//device_controller *control = dongle[1];
 	/*serial_scanner dongle(io, "ttyACM");
 	device_controller *control = dongle[1];*/
-	motor mot(control);
+	/*motor mot(control);
 	while (1)
 	{
 		cout << mot.encoder() << endl;
 		this_thread::sleep_for(chrono::milliseconds(2));
-	}
+	}*/
 
 	return module::type::menu;
 }
