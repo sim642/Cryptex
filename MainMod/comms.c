40622f4b81df9a38ca990962c57adf68bf743dcc
Simmo Saan <simmo.saan@gmail.com>
2015-10-16 10:33:19 +0300

    MainMod: clean up receive code

--- MainMod/comms.c
+++ MainMod/comms.c
@@ -8,6 +8,16 @@ volatile uint8_t usart_i = 0;
 volatile bool usart_data_ready = false;
 char reply[16];
 
+bool iseol(char ch)
+{
+	return ch == '\r' || ch == '\n';
+}
+
+bool isdata(char ch)
+{
+	return ch >= ' ' && ch <= '~';
+}
+
 void usart_init()
 {
 	UBRR1 = 51; // 19200, 8
@@ -26,9 +36,9 @@ void usb_write(const char *str)
 
 void usart_write(unsigned char* data)
 {
-	while(*data)
+	while (*data)
 	{
-		while(!bit_get(UCSR1A, BIT(UDRE1)));
+		while (!bit_get(UCSR1A, BIT(UDRE1)));
 		UDR1 = *data;
 		data++;
 	}
@@ -43,12 +53,13 @@ uint8_t usb_recv_str(char *buf, uint8_t size)
 	while (count < size)
 	{
 		data = usb_serial_getchar();
-		if (data == '\r' || data == '\n')
+
+		if (iseol(data))
 		{
 			*buf = '\0';
 			return size;
 		}
-		if (data >= ' ' && data <= '~')
+		else if (isdata(data))
 		{
 			*buf++ = data;
 			count++;
@@ -61,18 +72,15 @@ ISR(USART1_RX_vect)
 {
 	unsigned char ch = UDR1;
 
-	if(ch >= ' ' && ch <= '~')
-	{
-		usart_buf[usart_i] = ch;
-		usart_i++;
-	}
-
-	if(ch == '\n' || ch == '\r')
+	if (iseol(ch))
 	{
 		usart_buf[usart_i] = '\0';
 		usart_i = 0;
 		usart_data_ready = true;
-		return;
+	}
+	else if (isdata(ch))
+	{
+		usart_buf[usart_i++] = ch;
 	}
 }
 
