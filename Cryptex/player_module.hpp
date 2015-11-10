dbf7584eed333ac5ef2c7fc2b7cbeb9f4f5490f5
Simmo Saan <simmo.saan@gmail.com>
2015-11-10 16:58:57 +0200

    Specify fractional kick time in player

--- Cryptex/player_module.hpp
+++ Cryptex/player_module.hpp
@@ -35,7 +35,7 @@ class player_module : public module
 		const int btn_team = 1;
 		const double scalelow = 0.5;
 		const int dribblerspeed = 255;
-		const int kicktime = 3;
+		const double kicktime = 2.5;
 		const int kickstotal = 2;
 
 		void set_state(const state_t &new_state);
