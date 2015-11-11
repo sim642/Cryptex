eae4fad6751843059c43ef52e6e190a1e3ff7d62
Simmo Saan <simmo.saan@gmail.com>
2015-11-11 17:59:49 +0200

    Faster around ball rotation testing

--- Cryptex/test_module.cpp
+++ Cryptex/test_module.cpp
@@ -9,6 +9,7 @@
 #include "device_id.hpp"
 
 #include "rs485_dongle.hpp"
+#include "driver.hpp"
 #include "main_controller.hpp"
 
 using namespace std;
@@ -28,15 +29,19 @@ module::type test_module::run(const module::type &prev_module)
 	boost::asio::io_service io;
 
 	rs485_dongle dongle(io, "/dev/ttyUSB0");
-	main_controller mainer(dongle[device_id::main]);
+	driver d(dongle);
+	main_controller m(dongle[device_id::main]);
+
+	//m.dribbler(255);
+	//this_thread::sleep_for(chrono::seconds(1));
 
 	while (1)
 	{
-		mainer.ping(); // keep failsafe from triggering
-
-		cout << mainer.ball() << endl;
-
-		this_thread::sleep_for(chrono::milliseconds(100));
+		//m.ping();
+		d.omni(35, -85, 25);
+		//d.omni(45, -85, 32);
+		//d.omni(45, -80, 29);
+		this_thread::sleep_for(chrono::milliseconds(1000 / 60));
 	}
 
 	return module::type::menu;
