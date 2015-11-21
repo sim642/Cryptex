6fdf37a09517a596efb8fc2dd610754908d5faf7
Simmo Saan <simmo.saan@gmail.com>
2015-11-21 18:27:49 +0200

    Add line finding test

--- Cryptex/test_module.cpp
+++ Cryptex/test_module.cpp
@@ -31,24 +31,13 @@ test_module::~test_module()
 module::type test_module::run(const module::type &prev_module)
 {
 	global::env = "test";
-	cv::VideoCapture capture(0);
+	cv::VideoCapture capture(1);
 
-	calibrator_window calibrator(capture);
-	calibrator.calibrate("red", "cap");
-
-	cv::namedWindow("tracking");
-
-	blob_finder finder("red", "cap");
-	blob_tracker tracker(100);
-
-	cv::VideoWriter writer("tracking.avi",
-							//CV_FOURCC('M', 'J', 'P', 'G'),
-							CV_FOURCC('P', 'I', 'M', '1'),
-							30,
-							cv::Size(capture.get(CV_CAP_PROP_FRAME_WIDTH),
-										capture.get(CV_CAP_PROP_FRAME_HEIGHT)),
-							true);
-
+	cv::namedWindow("border");
+	cv::namedWindow("edge");
+	int lower = 0, upper = 255;
+	cv::createTrackbar("lower", "border", &lower, 512);
+	cv::createTrackbar("upper", "border", &upper, 512);
 
 	while (1)
 	{
@@ -56,27 +45,31 @@ module::type test_module::run(const module::type &prev_module)
 		cv::Mat frame;
 		capture >> frame;
 
+		cv::Mat gray;
+		cv::cvtColor(frame, gray, CV_BGR2GRAY);
+
+		cv::blur(gray, gray, cv::Size(3, 3));
+
+		cv::Mat canny;
+		cv::Canny(gray, canny, lower, upper);
+
 		cv::Mat display;
+		//cv::cvtColor(canny, display, CV_GRAY2BGR);
 		frame.copyTo(display);
 
-		blobs_t blobs;
-		finder.detect_frame(frame, blobs);
-		tracker.update(blobs);
-
-		for (auto &p : tracker.get_all())
+		vector<cv::Vec4i> lines;
+		cv::HoughLinesP(canny, lines, 1, CV_PI/180, 75, 100, 10);
+		for( size_t i = 0; i < lines.size(); i++ )
 		{
-			int id = p.first;
-			const blob &b = p.second;
-
-			cv::circle(display, b.kp.pt, b.kp.size / 2, cv::Scalar(0, 255, 0), 2);
-			cv::putText(display, to_string(id), b.kp.pt, cv::FONT_HERSHEY_PLAIN, 1.5, cv::Scalar(0, 0, 255), 2);
+			cv::Vec4i l = lines[i];
+			cv::line(display, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
 		}
 
-		imshow("tracking", display);
-		writer << display;
+		cv::imshow("border", display);
+		cv::imshow("edge", canny);
 
 		int dt = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - framestart).count();
-		char key = cv::waitKey(max(1, 1000 / 30 - dt));
+		char key = cv::waitKey(max(1, 1000 / 60 - dt));
 		switch (key)
 		{
 			case 'q':
