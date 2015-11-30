86c221a4e664362ad79c2ba66cabcf68cd8ac706
Simmo Saan <simmo.saan@gmail.com>
2015-11-30 18:42:49 +0200

    Revert "Move async reading to serial_device"
    This reverts commit c38700a37e8c0936294f508d479bf39ab24e9a41.

--- Cryptex/serial_device.cpp
+++ Cryptex/serial_device.cpp
@@ -1,5 +1,4 @@
 #include "serial_device.hpp"
-#include <boost/asio.hpp>
 
 serial_device::serial_device(boost::asio::serial_port &port) : serial_port(port)
 {
@@ -13,32 +12,7 @@ serial_device::~serial_device()
 
 std::streamsize serial_device::read(char *buf, std::streamsize n)
 {
-	auto &io = serial_port.get_io_service();
-	io.reset();
-
-	boost::asio::deadline_timer timeout(io);
-	timeout.expires_from_now(boost::posix_time::millisec(1));
-	timeout.async_wait([&](const boost::system::error_code &error)
-	{
-		if (error)
-			return;
-
-		serial_port.cancel();
-	});
-
-	size_t read = 0;
-	boost::asio::async_read(serial_port, boost::asio::buffer(buf, n), [&](const boost::system::error_code &error, size_t bytes_transferred)
-	{
-		if (error || !bytes_transferred)
-			return;
-
-		read = bytes_transferred;
-		timeout.cancel();
-	});
-
-	io.run();
-
-	return read;
+	return serial_port.read_some(boost::asio::buffer(buf, n));
 }
 
 std::streamsize serial_device::write(const char *buf, std::streamsize n)
